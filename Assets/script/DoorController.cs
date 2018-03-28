7d72cec4c67b8c75a93d5633796f888ef2de96f7
Unknown <am.oderwald@gmail.com>
2018-03-28 17:47:57 +0200

    first upload game

---
+++ Assets/script/DoorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            door.transform.position = new Vector3(-32, 7, 0);
+    }
+
+}
